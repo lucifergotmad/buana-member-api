488f6cb016bf64c7005aef38fe68cd95620439ee
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-02-01 10:38:27 +0700

    fix:[major] adding tukar poin controller

--- src/modules/tukar-poin/controller/tukar-poin.controller.ts
+++ src/modules/tukar-poin/controller/tukar-poin.controller.ts
@@ -1,8 +1,24 @@
+import { Body } from "@nestjs/common";
+import { ApiConflictResponse, ApiOkResponse } from "@nestjs/swagger";
 import { ControllerProperty } from "src/core/decorators/controller-decorators/class-decorators/controller-property.decorator";
+import { SecurePost } from "src/core/decorators/controller-decorators/class-decorators/secure-post.decorator";
+import { AuthUser } from "src/core/decorators/controller-decorators/param-decorators/auth-user.decorator";
+import { MessageResponseDTO } from "src/interface-adapter/dtos/message.response.dto";
+import { UserMongoEntity } from "src/modules/user/database/model/user.mongo-entity";
+import { CreateTukarPoin } from "../use-cases/create-tukar-poin.use-case";
+import { CreateTukarPoinRequestDTO } from "./dtos/create-tukar-poin.request.dto";
 
-@ControllerProperty("v1/tukar-poins", "Tukar Poins")
+@ControllerProperty("v1/tukar-poins", "[Transaksi] Tukar Poins")
 export class TukarPoinController {
-  constructor() {
-    // fill above parentheses with use case / repository dependencies
+  constructor(private readonly createTukarPoin: CreateTukarPoin) {}
+
+  @SecurePost()
+  @ApiOkResponse({ type: MessageResponseDTO })
+  @ApiConflictResponse({ description: "Data already exists" })
+  save(
+    @Body() body: CreateTukarPoinRequestDTO,
+    @AuthUser() user: Partial<UserMongoEntity>,
+  ) {
+    return this.createTukarPoin.injectDecodedToken(user).execute(body);
   }
 }
