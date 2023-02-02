226cb3fcdfe3adfa6ec33a9b82012aaf511f9a36
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-02-02 15:03:14 +0700

    fix:[major] update create member use case

--- src/modules/member/use-cases/create-member.use-case.ts
+++ src/modules/member/use-cases/create-member.use-case.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { BaseUseCase } from "src/core/base-classes/infra/use-case.base";
 import { IUseCase } from "src/core/base-classes/interfaces/use-case.interface";
 import { ResponseException } from "src/core/exceptions/response.http-exception";
@@ -29,49 +29,64 @@ export class CreateMember
       let result: IRepositoryResponse;
 
       await session.withTransaction(async () => {
-        const memberEntity = MemberEntity.create({
-          kode_member: await this.utils.generator.generateKodeMember(),
-          no_identitas: request.no_identitas,
-          tanggal_daftar: request.tanggal_daftar,
-          tanggal_valid: request.tanggal_valid,
-          nama_lengkap: request.nama_lengkap,
-          nama_depan: request?.nama_depan,
-          nama_belakang: request?.nama_belakang,
-          tanggal_lahir: request.tanggal_lahir,
-          tempat_lahir: request.tempat_lahir,
-          alamat: request.alamat,
-          rt_rw: request.rt_rw,
-          kelurahan: request.kelurahan,
-          kota: request.kota,
-          no_hp: request.no_hp,
-          kode_pos: request?.kode_pos,
-          domisili: request?.domisili,
-          status: request?.status,
-          agama: request?.agama,
-          pendidikan: request?.pendidikan,
-          penghasilan: request?.penghasilan,
-          pengeluaran: request?.pengeluaran,
-          jumlah_anak_pria: request?.jumlah_anak_pria,
-          jumlah_anak_wanita: request?.jumlah_anak_wanita,
-          pekerjaan: request?.pekerjaan,
-          nama_perusahaan: request?.nama_perusahaan,
-          alamat_perusahaan: request?.alamat_perusahaan,
-          kota_perusahaan: request?.kota_perusahaan,
-          bidang_usaha: request?.bidang_usaha,
-          emergency_number: request?.emergency_number,
-          jenis_kelamin: request?.jenis_kelamin,
-          terima_sms: request?.terima_sms,
-          telp_rumah: request?.telp_rumah,
-          telp_kantor: request?.telp_kantor,
-          email: request?.email,
-          terima_email: request?.terima_email,
-          poin: 0,
-          created_by: this?.user?.user_id,
-          status_active: true,
-          is_online: false,
-        });
+        const isExists = await this.memberRepository.findOne(
+          { no_identitas: request.no_identitas },
+          session,
+        );
+        if (isExists) {
+          if (!isExists.status_active) {
+            result = await this.memberRepository.update(
+              { no_identitas: request.no_identitas },
+              { status_active: true, created_by: this?.user?.user_id },
+            );
+          } else {
+            throw new BadRequestException("No identitas telah digunakan!");
+          }
+        } else {
+          const memberEntity = MemberEntity.create({
+            kode_member: await this.utils.generator.generateKodeMember(),
+            no_identitas: request.no_identitas,
+            tanggal_daftar: request.tanggal_daftar,
+            tanggal_valid: request.tanggal_valid,
+            nama_lengkap: request.nama_lengkap,
+            nama_depan: request?.nama_depan,
+            nama_belakang: request?.nama_belakang,
+            tanggal_lahir: request.tanggal_lahir,
+            tempat_lahir: request.tempat_lahir,
+            alamat: request.alamat,
+            rt_rw: request.rt_rw,
+            kelurahan: request.kelurahan,
+            kota: request.kota,
+            no_hp: request.no_hp,
+            kode_pos: request?.kode_pos,
+            domisili: request?.domisili,
+            status: request?.status,
+            agama: request?.agama,
+            pendidikan: request?.pendidikan,
+            penghasilan: request?.penghasilan,
+            pengeluaran: request?.pengeluaran,
+            jumlah_anak_pria: request?.jumlah_anak_pria,
+            jumlah_anak_wanita: request?.jumlah_anak_wanita,
+            pekerjaan: request?.pekerjaan,
+            nama_perusahaan: request?.nama_perusahaan,
+            alamat_perusahaan: request?.alamat_perusahaan,
+            kota_perusahaan: request?.kota_perusahaan,
+            bidang_usaha: request?.bidang_usaha,
+            emergency_number: request?.emergency_number,
+            jenis_kelamin: request?.jenis_kelamin,
+            terima_sms: request?.terima_sms,
+            telp_rumah: request?.telp_rumah,
+            telp_kantor: request?.telp_kantor,
+            email: request?.email,
+            terima_email: request?.terima_email,
+            poin: 0,
+            created_by: this?.user?.user_id,
+            status_active: true,
+            is_online: false,
+          });
 
-        result = await this.memberRepository.save(memberEntity, session);
+          result = await this.memberRepository.save(memberEntity, session);
+        }
       });
 
       return new IdResponseDTO(result._id);
