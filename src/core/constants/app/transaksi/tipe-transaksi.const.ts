9fea3640fdb12eebe1fd578ddb0fa6e63359dd9b
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-02-07 16:52:38 +0700

    fix:[minor] update tipe transaksi const

--- src/core/constants/app/transaksi/tipe-transaksi.const.ts
+++ src/core/constants/app/transaksi/tipe-transaksi.const.ts
@@ -2,5 +2,6 @@ export enum TipeTransaksi {
   TambahStockHadiah = "Tambah Stock Hadiah",
   AdjustStockHadiah = "Adjust Stock Hadiah",
   TukarPoin = "Tukar Poin",
-  SaldoAwal = "Saldo Awal",
+  Penjualan = "Penjualan",
+  UpdatePoin = "Update Poin",
 }
